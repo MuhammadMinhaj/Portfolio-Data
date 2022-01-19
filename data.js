2193ea9f48859356897783df9176b27a7071983a
Muhammad Minhaj <developer.mdminhaj@gmail.com>
2022-01-19 20:27:19 +0600

    feat: Added some data file

---
+++ data.js
@@ -0,0 +1,16 @@
+const data = [
+    {
+        id:1,
+        name:"Minhaj"
+    },
+    {
+        id:2,
+        name:"Ashek"
+    },
+    {
+        id:3,
+        name:"Towsif"
+    },
+]
+
+export default data 
